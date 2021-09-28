60f27cc0f5529985584debc1ae8df636869eb4a4
Sofiya Kryuchkova <sofiakru123@gmail.com>
2021-09-28 11:56:52 +0100

    16 - Update Big Cup

--- 16-drink-water/script.js
+++ 16-drink-water/script.js
@@ -3,6 +3,8 @@ const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
 
+updateBigCup();
+
 smallCups.forEach((cup, index) => {
   cup.addEventListener("click", () => {
     highlightCups(index);
@@ -24,4 +26,28 @@ function highlightCups(index) {
       cup.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerHTML = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
