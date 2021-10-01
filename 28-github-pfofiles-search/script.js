0bbfe393a290778009fc4dec38d3f7cb15587339
Sofiya Kryuchkova <sofiakru123@gmail.com>
2021-10-01 15:02:06 +0100

    28 - Dynamic Profile Cards

--- 28-github-pfofiles-search/script.js
+++ 28-github-pfofiles-search/script.js
@@ -1,15 +1,54 @@
 const API_URL = "https://api.github.com/users/";
 
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
 async function getUser(username) {
   try {
     const { data } = await axios(API_URL + username);
-    console.log(data);
+    createUserCard(data);
   } catch (err) {
-    console.log(err);
+    if (err.response.status == 404) {
+      createErrorCard("No profile with this username found");
+    }
   }
+}
+
+function createUserCard(user) {
+  const cardHTML = ` <div class="card">
+<div>
+  <img src="${user.avatar_url}" alt="${user.name}" class="avatar" />
+</div>
+<div class="user-info">
+  <h2>${user.name}</h2>
+  <p>
+    ${user.bio}
+  </p>
+  <ul>
+    <li>${user.followers} <strong>Followers</strong></li>
+    <li>${user.following} <strong>Following</strong></li>
+    <li>${user.public_repos} <strong>Repos</strong></li>
+  </ul>
+
+  <div id="repos">
+    <a href="#" class="repo">Repo 1</a>
+    <a href="#" class="repo">Repo 2</a>
+    <a href="#" class="repo">Repo 3</a>
+  </div>
+</div>
+</div>`;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+        <h2>${msg}</h2>
+    </div>
+    `;
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener("submit", (e) => {
