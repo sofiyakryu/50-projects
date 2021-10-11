c40bfcfb8b3c8ecfa0d1d5a320d76732f55aa05b
Sofiya Kryuchkova <sofiakru123@gmail.com>
2021-10-11 15:50:31 +0100

    37 - Creating The Pokemon Cards

--- 37-pokedex/script.js
+++ 37-pokedex/script.js
@@ -1,4 +1,4 @@
-const poke_container = document.getElementById("poke-cotainer");
+const poke_container = document.getElementById("poke-container");
 const pokemon_count = 150;
 const colors = {
   fire: "#fddfdf",
@@ -17,6 +17,8 @@ const colors = {
   normal: "#F5F5F5",
 };
 
+const main_types = Object.keys(colors);
+
 const fetchPokemons = async () => {
   for (let i = 1; i <= pokemon_count; i++) {
     await getPokemon(i);
@@ -27,7 +29,37 @@ const getPokemon = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  createPokemonCard(data);
+};
+
+//for each pokemon id that we fetch it will create a card =>
+//if we had many results it wouldn't be efficient beacuse we're making all these separate requests, but since it's not a production app..
+const createPokemonCard = (pokemon) => {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const id = pokemon.id.toString().padStart(3, "0");
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+  const poke_types = pokemon.types.map((type) => type.type.name);
+  const type = main_types.find((type) => poke_types.indexOf(type) > -1);
+  const color = colors[type];
+
+  pokemonEl.style.backgroundColor = color;
+
+  const pokemonInnerHTML = `
+    <div class="img-container">
+          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="" />
+    </div>
+    <div class="info">
+          <span class="number">#${id}</span>
+          <h3 class="name">${name}</h3>
+          <small class="type">Type: <span>${type}</span></small>
+    </div>    
+    `;
+
+  pokemonEl.innerHTML = pokemonInnerHTML;
+
+  poke_container.appendChild(pokemonEl);
 };
 
 fetchPokemons();
